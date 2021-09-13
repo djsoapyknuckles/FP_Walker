62eaa60ae8dcf9e664afc611acead1bb44a6e33e
djsoapyknuckles@gmail.com <djsoapyknuckles@gmail.com>
2021-09-13 09:08:10 -0400

    updated FP_Walker methods

--- FP_Walker.cs
+++ FP_Walker.cs
@@ -1,52 +1,130 @@
 using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
-public class FP_Walker : MVRScript
+
+
+namespace djsoapyknuckles.FP_Walker
+
 {
-    public override void Init()
+    public class FP_Walker : MVRScript
     {
-        try
-        {
-            SuperController.LogMessage($"{nameof(MyPlugin)} initialized");
-        }
-        catch (Exception e)
-        {
-            SuperController.LogError($"{nameof(MyPlugin)}.{nameof(Init)}: {e}");
-        }
-    }
 
-    public void OnEnable()
-    {
-        try
-        {
-            SuperController.LogMessage($"{nameof(MyPlugin)} enabled");
-        }
-        catch (Exception e)
-        {
-            SuperController.LogError($"{nameof(MyPlugin)}.{nameof(OnEnable)}: {e}");
-        }
-    }
 
-    public void OnDisable()
-    {
-        try
-        {
-            SuperController.LogMessage($"{nameof(MyPlugin)} disabled");
-        }
-        catch (Exception e)
-        {
-            SuperController.LogError($"{nameof(MyPlugin)}.{nameof(OnDisable)}: {e}");
-        }
-    }
 
-    public void OnDestroy()
-    {
-        try
+
+        //UI
+        #region Variables
+
+        //private Camera _mainCamera;
+        private float camera_pos_x;
+        private FreeControllerV3 _rootControl;
+        private JSONStorableFloat _offset;
+        private bool _isMoving = false;
+        private Camera _mainCamera;
+        
+        
+        
+
+        //public BVHPlayer animator;
+
+        #endregion
+
+        public override void Init()
         {
-            SuperController.LogMessage($"{nameof(MyPlugin)} destroyed");
+            try
+            {
+                _mainCamera = SuperController.singleton.OVRCenterCamera;
+                _rootControl = containingAtom.mainController;
+                SuperController.LogMessage("root control is at position x: " + _rootControl.transform.position.x.ToString());
+                StartCoroutine(InitDeferred());
+            }
+
+            catch (Exception e)
+            {
+                SuperController.LogError("Exception caught: " + e);
+            }
+
         }
-        catch (Exception e)
+
+        private IEnumerator CheckMoving()
         {
-            SuperController.LogError($"{nameof(MyPlugin)}.{nameof(OnDestroy)}: {e}");
+            while (true)
+            {
+                Vector3 startPos = _mainCamera.transform.position;
+                //SuperController.LogMessage("start: " + startPos.ToString());
+                yield return new WaitForSeconds(0.25f);
+                Vector3 finalPos = _mainCamera.transform.position;
+                //SuperController.LogMessage("final: " + finalPos.ToString());
+                if (_rootControl != null)
+                {
+                    if //(startPos.x != finalPos.x || startPos.z != finalPos.z)
+                        (Math.Abs(startPos.x - finalPos.x) > 0.2 || Math.Abs(startPos.z - finalPos.z) > 0.2) //differentiate between rotational movement vs. linear movement
+                    {
+
+                       //play bvh animation here
+                        SuperController.LogMessage("CheckMoving: animation should be playing");
+                    }
+                    else if (startPos.x == finalPos.x && startPos.z == finalPos.z)
+                    {
+                        //stop playing bvh animation here
+                        SuperController.LogMessage("CheckMoving: playback should be stopped");
+                        
+                    }
+
+                    else
+                    {
+                        SuperController.LogMessage("no -if- condition met: playback not started");
+                    }
+
+                }
+
+            }
         }
+
+        void FixedUpdate()
+        {
+
+
+            if (_rootControl.transform.position != _mainCamera.transform.position)
+            {
+                //translate rootControl to camera position in x/z
+                _rootControl.transform.position = new Vector3(_mainCamera.transform.position.x, 0, _mainCamera.transform.position.z);
+
+            }
+
+            if (_rootControl.transform.rotation.eulerAngles.y != _mainCamera.transform.rotation.eulerAngles.y)
+            {
+                //rotate rootControl around y axix to match camera rotation
+                _rootControl.transform.rotation = Quaternion.Euler(new Vector3(0, _mainCamera.transform.eulerAngles.y, 0));
+            }
+        }
+       
+        
+        protected IEnumerator InitDeferred()
+        {
+            //may not need initDeffered
+            yield return new WaitForEndOfFrame();
+            //SuperController.LogMessage("in initDefered");
+            yield return new WaitForEndOfFrame();
+            while (SuperController.singleton.isLoading)
+            {
+                //SuperController.LogMessage("is loading...");
+                yield return null;
+            }
+            //_mam = SuperController.singleton.motionAnimationMaster;
+
+
+            SuperController.LogMessage(_mainCamera.ToString());
+
+            StartCoroutine(CheckMoving());
+            //SuperController.LogMessage("out of initDefered");
+        }
+
+
     }
 }
